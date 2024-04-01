377de77054ca64637cd3fa51d7431466f7817c7b
larisa <larisa.mih2004@gmail.com>
2024-04-01 23:08:32 +0300

    create migration

--- catalog/models.py
+++ catalog/models.py
@@ -17,13 +17,13 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=200, verbose_name='Наименование')
-    description = models.TextField(verbose_name='Описание')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
     image = models.ImageField(upload_to='products/', verbose_name='Изображение', **NULLABLE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='id')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
     price = models.IntegerField(verbose_name='Цена за покупку')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
-    # manufactured_at = models.DateField(null=True, blank=True, verbose_name='Дата производства продукта')
+
 
     def __str__(self):
         return f'Имя продукта: {self.name}  Категория:{self.category}   Цена: {self.price} рублей'
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,7 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path
 from catalog.views import index_home, index_contacts
+
 urlpatterns = [
+    path('', index_home),
     path('contacts/', index_contacts),
-    path('', index_home)
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
