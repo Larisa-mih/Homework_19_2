514a0756e6186466b0e35513d6737727c35d4c99
larisa <larisa.mih2004@gmail.com>
2024-04-08 07:51:05 +0300

    modified

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -40,14 +40,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        Category.objects.all().delete()
-        Product.objects.all().delete()
-        Contacts.objects.all().delete()
-
         with connection.cursor() as cursor:
             cursor.execute(
                 "TRUNCATE TABLE catalog_category, catalog_product, catalog_contacts RESTART IDENTITY CASCADE;")
-
 
         product_for_create = []
         category_for_create = []
@@ -55,32 +50,32 @@ class Command(BaseCommand):
 
         for category in Command.json_read_categories():
             category_for_create.append(
-            Category(id=category['pk'],
-                     name=category["fields"]["name"],
-                     description=category["fields"]["description"])
-        )
+                Category(id=category['pk'],
+                         name=category["fields"]["name"],
+                         description=category["fields"]["description"])
+            )
         Category.objects.bulk_create(category_for_create)
 
         for product in Command.json_read_products():
             product_for_create.append(
-            Product(id=product['pk'],
-                    name=product["fields"]["name"],
-                    description=product["fields"]["description"],
-                    image=product["fields"]["image"],
-                    category=Category.objects.get(pk=product["fields"]["category"]),
-                    price=product["fields"]["price"],
-                    created_at=product["fields"]["created_at"],
-                    updated_at=product["fields"]["updated_at"])
+                Product(id=product['pk'],
+                        name=product["fields"]["name"],
+                        description=product["fields"]["description"],
+                        image=product["fields"]["image"],
+                        category=Category.objects.get(pk=product["fields"]["category"]),
+                        price=product["fields"]["price"],
+                        created_at=product["fields"]["created_at"],
+                        updated_at=product["fields"]["updated_at"])
             )
 
         Product.objects.bulk_create(product_for_create)
 
         for contact in Command.json_read_contacts():
             contact_for_create.append(
-            Contacts(id=contact['pk'],
-                     first_name=contact["fields"]["first_name"],
-                     last_name=contact["fields"]["last_name"],
-                     email=contact["fields"]["email"])
+                Contacts(id=contact['pk'],
+                         first_name=contact["fields"]["first_name"],
+                         last_name=contact["fields"]["last_name"],
+                         email=contact["fields"]["email"])
             )
 
         Contacts.objects.bulk_create(contact_for_create)
