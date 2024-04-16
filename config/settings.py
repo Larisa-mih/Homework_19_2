d08317677d3c9db079df6429279a51a7dc162b8b
larisa <larisa.mih2004@gmail.com>
2024-04-16 06:51:14 +0300

    modified config

--- config/settings.py
+++ config/settings.py
@@ -35,8 +35,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'catalog'
+    'catalog',
+    'article',
 ]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
